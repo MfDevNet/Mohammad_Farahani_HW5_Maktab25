9f5a38d195794156956058e0e06cf1a0c6d66449
MfDevNet <MfDevNet@gmail.com>
2019-03-22 22:09:08 +0430

    declare getter

--- shop/Product.java
+++ shop/Product.java
@@ -9,6 +9,8 @@ public abstract class Product {
     private long p_Price;
     private String p_Description;
 
+    Product(){}
+
     public Product(String p_Id, String p_Name, String p_Brand, int p_Quantity, long p_Price, String p_Description) {
         this.p_Id = p_Id;
         this.p_Name = p_Name;
@@ -16,5 +18,30 @@ public abstract class Product {
         this.p_Quantity = p_Quantity;
         this.p_Price = p_Price;
         this.p_Description = p_Description;
+    }
+
+
+    public String getP_Id() {
+        return p_Id;
+    }
+
+    public String getP_Name() {
+        return p_Name;
+    }
+
+    public String getP_Brand() {
+        return p_Brand;
+    }
+
+    public int getP_Quantity() {
+        return p_Quantity;
+    }
+
+    public long getP_Price() {
+        return p_Price;
+    }
+
+    public String getP_Description() {
+        return p_Description;
     }
 }
