1bbd14e3bf629bbfe425ea18a66408e6ee952daf
MfDevNet <MfDevNet@gmail.com>
2019-03-24 21:33:31 +0430

    implement class abstract NewspaperSubscription

--- NewspaperSubscription/NewspaperSubscription.java
+++ NewspaperSubscription/NewspaperSubscription.java
@@ -1,4 +1,32 @@
 package NewspaperSubscription;
 
-public class NewspaperSubscription {
+public abstract class NewspaperSubscription {
+
+    private String subscriberName;
+    protected String address;
+    private double subscriptionRate;
+
+    public void setSubscriberName(String subscriberName) {
+        this.subscriberName = subscriberName;
+    }
+
+    public String getSubscriberName() {
+        return subscriberName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public double getSubscriptionRate() {
+        return subscriptionRate;
+    }
+
+    public void setSubscriptionRate(double subscriptionRate) {
+        this.subscriptionRate = subscriptionRate;
+    }
+
+    public abstract String setAddress(String address);
+
+
+
+
 }
