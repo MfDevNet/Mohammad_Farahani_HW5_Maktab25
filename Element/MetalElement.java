aac6ab8577ed34fc4bd544ddfc1e38a7f909f979
MfDevNet <MfDevNet@gmail.com>
2019-03-25 02:14:10 +0430

    implement class metal and nonmetal constructor with 3 parameter override method describeElement

--- Element/MetalElement.java
+++ Element/MetalElement.java
@@ -1,4 +1,17 @@
 package Element;
 
-public class MetalElement {
+
+public class MetalElement extends Element {
+
+    // constructor with 3 parameter
+    public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+
+    // override method describeElement in super class
+    @Override
+    public String describeElement() {
+        return(super.getSymbol() + " is a metal element. It has an atomic number of " + super.getAtomicNumber() + " and a weight of " + super.getAtomicWeight() );
+    }
+
 }
