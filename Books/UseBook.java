1c80ad3919ba611be6bc3dfad14ed951290861be
MfDevNet <MfDevNet@gmail.com>
2019-03-24 14:08:31 +0430

    implement usebook class for assigment and display book

--- Books/UseBook.java
+++ Books/UseBook.java
@@ -1,4 +1,25 @@
 package Books;
 
 public class UseBook {
+    public static void main(String[] args) {
+        Book Books[] = new Book[10];
+
+        for (int i = 0; i < Books.length; i++) {
+            if (i % 2 == 0) {
+                Books[i] = new Fiction("Fiction Book" + i);
+
+            } else {
+                Books[i] = new NonFiction("NonFiction Book" + i);
+
+            }
+
+        }
+        for (Book book : Books) {
+            System.out.println("Book: " + book.getTitle() + " Price $" + book.getPrice());
+        }
+    }
 }
+
+
+
+
