83c625510cb933360ec782b51ea23c2488298aea
MfDevNet <MfDevNet@gmail.com>
2019-03-24 19:53:07 +0430

    implement super class abstract phonecell

--- TalkALot/PhoneCall.java
+++ TalkALot/PhoneCall.java
@@ -2,8 +2,21 @@ package TalkALot;
 
 public abstract class PhoneCall {
 
+    //Declare variables.
+    String phoneNumber;
+    double callPrice;
 
+    //Constructor.
+    public PhoneCall(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        callPrice = 0.0;
+    }
 
-
-
-}//End of PhoneCall class
+    public void setCallPrice(double callPrice) {
+        this.callPrice = callPrice;
+    }
+    // abstract method
+    public abstract String getPhoneNumber();
+    public abstract double getPrice();
+    public abstract void displayInfo();
+}
