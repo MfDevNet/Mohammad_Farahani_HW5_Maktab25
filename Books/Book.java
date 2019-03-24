cddef82f219b9a6146a268e8b300c403bc86bfa6
MfDevNet <MfDevNet@gmail.com>
2019-03-24 14:06:39 +0430

    declare variable and constructor book class

--- Books/Book.java
+++ Books/Book.java
@@ -1,4 +1,27 @@
 package Books;
 
-public class Book {
+public abstract class Book {
+    private String title;
+    double price;
+
+    public Book(String title){
+        this.title=title;
+        this.price=0.0;
+
+
+    }
+
+    public String getTitle(){
+        return this.title;
+
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public abstract void setPrice();
+
+
 }
+
