ed7b4169071db0ea0cf29ddcee42f7caad871be6
MfDevNet <MfDevNet@gmail.com>
2019-03-24 14:07:25 +0430

    implement 2 class fiction and nonfiction

--- Books/Fiction.java
+++ Books/Fiction.java
@@ -1,4 +1,17 @@
 package Books;
 
-public class Fiction {
+public class Fiction extends Book {
+
+
+    public Fiction(String title) {
+        super(title);
+        setPrice();
+    }
+
+    public void setPrice() {
+        super.price = 24.99;
+
+    }
+
+
 }
--- Books/NonFiction.java
+++ Books/NonFiction.java
@@ -1,4 +1,17 @@
 package Books;
 
-public class NonFiction {
+public class NonFiction extends Book {
+
+    public NonFiction(String title) {
+        super(title);
+        setPrice();
+    }
+
+    public void setPrice() {
+        super.price = 37.99;
+
+    }
+
+
+
 }
